df4f38ac62ec65d537bfb4dea3c8ec2b2ef2aea9
Abhisek Pradhan <125997876+AbhisekPradhan824@users.noreply.github.com>
2026-03-27 10:45:18 +0530

    Create Tester.java

---
+++ SwiftFood/src/Part-5/Assignment-2/Tester.java
@@ -0,0 +1,25 @@
+package com.infy;
+
+import java.util.Scanner;
+
+public class Tester {
+
+	public static void main(String[] args) {
+		Scanner scn =new Scanner(System.in);
+		Resturant r1 = new Resturant();
+		
+		System.out.println("Enter Resturant Name");
+		r1.resturantName = scn.nextLine();
+		System.out.println("Enter Rating: ");
+		r1.rating=scn.nextFloat();
+		System.out.println("Enter Contact of Resturant");
+		r1.resturantContact=scn.nextLong();
+		System.out.println("Enter Address");
+		r1.resturantAddress = scn.nextLine();
+		
+		r1.displayResturantDetails();
+		scn.close();
+
+	}
+
+}
