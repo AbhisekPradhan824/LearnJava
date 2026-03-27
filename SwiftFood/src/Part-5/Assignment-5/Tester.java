5eae7eb235f40004c013c86a4dee78009c95eb24
Abhisek Pradhan <125997876+AbhisekPradhan824@users.noreply.github.com>
2026-03-27 16:20:32 +0530

    Create Tester.java

---
+++ SwiftFood/src/Part-5/Assignment-5/Tester.java
@@ -0,0 +1,17 @@
+package com.infy;
+
+public class Tester {
+
+	public static void main(String[] args) {
+		Order order1=new Order();
+		System.out.println("Status of order 1: "+ order1.status);
+		
+		Order order2 = new Order(1001, "Garlic Shrimp");
+		System.out.println("Status of order 1: "+ order2.status);
+		System.out.println("Id of order 2: "+order2.orderId);
+		System.out.println("Items ordered in order 2: "+order2.orderedFoods);
+		
+
+	}
+
+}
