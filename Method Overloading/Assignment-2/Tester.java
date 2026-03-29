5cac296af9b77f295c8f18e23f972396a5085fb9
Abhisek Pradhan <125997876+AbhisekPradhan824@users.noreply.github.com>
2026-03-29 15:10:16 +0530

    Create Tester.java

---
+++ Method Overloading/Assignment-2/Tester.java
@@ -0,0 +1,28 @@
+class Tester {
+
+	public static void main(String[] args) {
+		Triangle triangle1 = new Triangle();
+		Triangle triangle2 = new Triangle(1, 2, 6, 5, 5, 1);
+		
+		Point point1 = new Point(2, 1);
+		Point point2 = new Point(4, 4);
+		Point point3 = new Point(9, 1);
+		Triangle triangle3 = new Triangle(point1, point2, point3);		
+		Triangle triangle4 = new Triangle(3.0, 3.0, -1.0, -1.0, 4.0, -4.0);
+		System.out.println("Perimeter of triangle1 is "+triangle1.calculatePerimeter());
+		System.out.println("Area of triangle1 is "+triangle1.calculateArea());
+		
+		System.out.println("Perimeter of triangle2 is "+triangle2.calculatePerimeter());
+		System.out.println("Area of triangle2 is "+triangle2.calculateArea());
+		
+		System.out.println("Perimeter of triangle3 is "+triangle3.calculatePerimeter());
+		System.out.println("Area of triangle3 is "+triangle3.calculateArea());
+		
+		//Create more objects of Triangle class for testing your code      
+		System.out.println("Perimeter of triangle4 is "+triangle4.calculatePerimeter());
+		System.out.println("Area of triangle4 is "+triangle4.calculateArea());
+
+	}
+}
+
+
