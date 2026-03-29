7b1db97595db1ea5cec381aa5b9d958b1a0d96b0
Abhisek Pradhan <125997876+AbhisekPradhan824@users.noreply.github.com>
2026-03-29 15:09:37 +0530

    Create Triangle.java

---
+++ Method Overloading/Assignment-2/Triangle.java
@@ -0,0 +1,67 @@
+
+class Triangle {
+    private Point point1;
+    private Point point2;
+    private Point point3;
+    //Implement your code here
+    
+   public Triangle(){
+        point1=new Point(0,0);
+        point2=new Point(1,1);
+        point3 = new Point(2,5);
+    }
+    public Triangle(double point1XCoordinate, double point1YCoordinate, double point2XCoordinate, double point2YCoordinate, double point3XCoordinate, double point3YCoordinate){
+        point1 = new Point(point1XCoordinate, point1YCoordinate);
+        point2 = new Point(point2XCoordinate, point2YCoordinate);
+        point3 = new Point(point3XCoordinate, point3YCoordinate);
+    }
+    public Triangle(Point point1, Point point2, Point point3){
+        this.point1 = point1;
+        this.point2 = point2;
+        this.point3 = point3;
+    }
+    
+    
+    public void setPoint1(Point point1){
+        this.point1=point1;
+    }
+    public Point getPoint1(){
+        return point1;
+    }
+    
+    
+    public void setPoint2(Point point2){
+        this.point2=point2;
+    }
+    public Point getPoint2(){
+        return point2;
+    }
+    
+    
+    public void setPoint3(Point point3){
+        this.point3=point3;
+    }
+    public Point getPoint3(){
+        return point3;
+    }
+    
+    
+    
+    public double calculatePerimeter(){
+    	double c =point1.calculateDistance(point2);
+    	double b =point1.calculateDistance(point3);
+    	double a =point2.calculateDistance(point3);
+    	return Math.round((a+b+c)*100.0)/100.0;
+        
+    }
+	public double calculateArea() {
+		double c =point1.calculateDistance(point2);
+    	double b =point1.calculateDistance(point3);
+    	double a =point2.calculateDistance(point3);
+    	double s =calculatePerimeter()/2.0;
+		double area=Math.sqrt(s*(s-a)*(s-b)*(s-c));
+		return Math.round(area*100.0)/100.0;
+	}
+    
+    
+}
